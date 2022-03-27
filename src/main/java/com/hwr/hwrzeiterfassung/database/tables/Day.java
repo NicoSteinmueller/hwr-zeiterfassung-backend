2862544b4adb7af70a85824b318b800fad2b18e0
Stier-09 <82510949+Stier-09@users.noreply.github.com>
2022-03-27 23:02:14 +0200

    replace unsafe annotation

--- src/main/java/com/hwr/hwrzeiterfassung/database/tables/Day.java
+++ src/main/java/com/hwr/hwrzeiterfassung/database/tables/Day.java
@@ -1,14 +1,16 @@
 package com.hwr.hwrzeiterfassung.database.tables;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Date;
 import java.util.Set;
 
 @Entity
-@Data
+@NoArgsConstructor
+@Getter
 @Table
 public class Day {
     @Id
