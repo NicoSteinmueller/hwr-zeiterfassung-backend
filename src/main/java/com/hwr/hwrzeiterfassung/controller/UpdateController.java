eddd99606136b4c6f880922ef54aa8b5aacbf782
Stier-09 <82510949+Stier-09@users.noreply.github.com>
2022-04-26 23:02:45 +0200

    correct Response HttpStatus

--- src/main/java/com/hwr/hwrzeiterfassung/controller/UpdateController.java
+++ src/main/java/com/hwr/hwrzeiterfassung/controller/UpdateController.java
@@ -6,11 +6,11 @@ import com.hwr.hwrzeiterfassung.database.repositorys.LoginRepository;
 import com.hwr.hwrzeiterfassung.database.repositorys.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 /**
@@ -37,15 +37,14 @@ public class UpdateController {
      * @return HttpStatus Accepted or Not_Acceptable
      */
     @PostMapping(path = "/password")
-    public @ResponseStatus
-    HttpStatus password(@RequestParam String email, @RequestParam String password, @RequestParam String newPassword) {
+    public ResponseEntity<HttpStatus> password(@RequestParam String email, @RequestParam String password, @RequestParam String newPassword) {
         loginController.validateLoginInformation(email, password);
         if (newPassword.length() != 256)
             throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Hash from new Password is not valid.");
         var login = loginRepository.getById(email);
         login.setPassword(newPassword);
         loginRepository.saveAndFlush(login);
-        return HttpStatus.ACCEPTED;
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
     /**
@@ -57,13 +56,12 @@ public class UpdateController {
      * @return HttpStatus Accepted or Not_Acceptable
      */
     @PostMapping(path = "/lastName")
-    public @ResponseStatus
-    HttpStatus lastName(@RequestParam String email, @RequestParam String password, @RequestParam String newLastName) {
+    public ResponseEntity<HttpStatus> lastName(@RequestParam String email, @RequestParam String password, @RequestParam String newLastName) {
         loginController.validateLoginInformation(email, password);
         var human = humanRepository.getById(email);
         human.setLastName(newLastName);
         humanRepository.saveAndFlush(human);
-        return HttpStatus.ACCEPTED;
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
     /**
@@ -75,13 +73,12 @@ public class UpdateController {
      * @return HttpStatus Accepted or Not_Acceptable
      */
     @PostMapping(path = "/targetDailyWorkingTime")
-    public @ResponseStatus
-    HttpStatus targetDailyWorkingTime(@RequestParam String email, @RequestParam String password, @RequestParam double targetDailyWorkingTime) {
+    public ResponseEntity<HttpStatus> targetDailyWorkingTime(@RequestParam String email, @RequestParam String password, @RequestParam double targetDailyWorkingTime) {
         loginController.validateLoginInformation(email, password);
         var human = humanRepository.getById(email);
         human.setTargetDailyWorkingTime(targetDailyWorkingTime);
         humanRepository.saveAndFlush(human);
-        return HttpStatus.ACCEPTED;
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
     /**
@@ -93,13 +90,12 @@ public class UpdateController {
      * @return HttpStatus Accepted or Not_Acceptable
      */
     @PostMapping(path = "/defaultProjectId")
-    public @ResponseStatus
-    HttpStatus defaultProjectId(@RequestParam String email, @RequestParam String password, @RequestParam int projectId) {
+    public ResponseEntity<HttpStatus> defaultProjectId(@RequestParam String email, @RequestParam String password, @RequestParam int projectId) {
         loginController.validateLoginInformation(email, password);
         var human = humanRepository.getById(email);
         human.setDefaultProject(projectRepository.getById(projectId));
         humanRepository.saveAndFlush(human);
-        return HttpStatus.ACCEPTED;
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
 }
