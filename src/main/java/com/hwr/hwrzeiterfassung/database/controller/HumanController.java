2baa65c6f95298abd094466434d73a288a1de821
Stier-09 <82510949+Stier-09@users.noreply.github.com>
2022-05-24 20:21:40 +0200

    #94 Optional for get default Project and add report site plugin

--- src/main/java/com/hwr/hwrzeiterfassung/database/controller/HumanController.java
+++ src/main/java/com/hwr/hwrzeiterfassung/database/controller/HumanController.java
@@ -14,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * Controller for the human
@@ -71,10 +72,10 @@ public class HumanController {
      */
     @GetMapping(path = "/getDefaultProject")
     public @ResponseBody
-    Project getDefaultProject(@RequestParam String email, @RequestParam String password) {
+    Optional<Project> getDefaultProject(@RequestParam String email, @RequestParam String password) {
         loginController.validateLoginInformation(email, password);
 
-        return getHumanByEmail(email).getDefaultProject();
+        return Optional.ofNullable(getHumanByEmail(email).getDefaultProject());
     }
 
     /**
